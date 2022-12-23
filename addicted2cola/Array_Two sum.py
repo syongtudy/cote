3cb544f4ddfa7db4eb3b9f56c99ed750660cd050
addicted2cola <jecahn@naver.com>
2022-12-23 13:32:03 +0900

    22/12/23

--- addicted2cola/Array_Two sum.py
+++ addicted2cola/Array_Two sum.py
@@ -1,13 +1,13 @@
 class Solution:
     def twoSum(self, nums, target: int):
         C={}    #딕셔너리가 탐색속도가 훨씬 빠름
-        for i in range(len(nums)):  #O(N)
-            if nums[i] in C:    #딕셔너리에 키가 있는지 확인 O(1)
-                return([C[nums[i]],i])
+        for i in range(len(nums)):  
+            if nums[i] in C:    
+                return([C[nums[i]],i])  #target-nums값과 위치 저장해둔다 ex) [2,7,11,15],9 의 경우 C={7:0,2:1,-2,2,-6,3}
             else:
-                C[target-nums[i]] = i   #키값 삽입도 O(1)
+                C[target-nums[i]] = i
 
-#총 O(N), 이중 for문 O(N^2)보다 빠름
+#nums를 돌며 딕셔너리 확인 및 삽임, O(N) x O(1) = 총 O(N), 이중 for문 O(N^2)보다 빠름
 
 # s = Solution()
 # print(s.twoSum([2,7,11,15],9))
