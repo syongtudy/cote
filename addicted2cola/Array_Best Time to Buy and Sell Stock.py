800a72821020a9013d61528f84662c70d8aefb30
addicted2cola <jecahn@naver.com>
2022-12-23 15:21:05 +0900

    22/12/23

--- addicted2cola/Array_Best Time to Buy and Sell Stock.py
+++ addicted2cola/Array_Best Time to Buy and Sell Stock.py
@@ -1,10 +1,10 @@
 class Solution:
     def maxProfit(self, prices):
-        d = [[0,prices[0]]]
+        d = [[0,prices[0]]] #d[x] = [x일 이전에 팔았을 때 얻을수있는 최대 이득, x까지의 최소 가격]
         for i in range(1,len(prices)):
-            d.append([max(d[i-1][0],prices[i]-d[i-1][1]),min(d[i-1][1],prices[i])])
+            d.append([max(d[i-1][0],prices[i]-d[i-1][1]),min(d[i-1][1],prices[i])]) #최대이득, 최소가격 갱신 O(1)
         return(d[-1][0])
-
+# prices 한번순회 O(N)
 
 # s = Solution()
 # print(s.maxProfit([7,1,5,3,6,4]))
