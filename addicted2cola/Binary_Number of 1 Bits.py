aa22c479b07bf1cc1bb3510b10a101178b01dc48
addicted2cola <jecahn@naver.com>
2022-12-30 23:10:00 +0900

    22/12/30

---
+++ addicted2cola/Binary_Number of 1 Bits.py
@@ -0,0 +1,10 @@
+# 리트코드의 문제에서는 입력을 binary string으로 주겠다고 했지만 실제로 run 돌려보면 함수에 들어오는 값은 int라서 문제 생겼엇음
+
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        return bin(n).count("1")
+
+s=Solution()
+print(s.hammingWeight(format(100,"032b")))
+
